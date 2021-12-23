fa370d63fbc7070c3f646add9da4c7b879130366
GuoJiatao <1570601232@qq.com>
2021-12-23 21:31:47 +0800

    add a line

--- src/dataset_analyze.py
+++ src/dataset_analyze.py
@@ -1,6 +1,49 @@
 import pandas as pd
+import time
 import numpy as np
+from scipy.sparse import data
 from tqdm.notebook import tqdm
+
+import datetime
+
+date_list = []
+dt = datetime.datetime.strptime('2017-11-25', "%Y-%m-%d")
+date = '2017-11-25'
+while date <= '2017-12-03':
+    date_list.append(date)
+    dt = dt + datetime.timedelta(1)
+    date = dt.strftime("%Y-%m-%d")
+
+def get_unixtime(timeStr):
+    formatStr = "%Y-%m-%d %H:%M:%S"
+    tmObject = time.strptime(timeStr, formatStr)
+    tmStamp = time.mktime(tmObject)
+
+    return int(tmStamp)
+
+
+# 数据集描述的时间范围
+startTime = get_unixtime("2017-11-25 00:00:00")
+endTime = get_unixtime("2017-12-3 23:59:59")
+chunk_size = 100000  # chunk_size
+chunks = []
+
+df = pd.concat(chunks, ignore_index=True)
+# 筛选
+df_out = df.loc[(df['TimeStamps'] >= startTime) & (df['TimeStamps'] <= endTime)]
+
+# 抽取子集
+df_out = df_out.sample(frac=0.2, random_state=1)
+
+# 去重前的数据维度
+print(df.shape)  # (100150807, 5)
+# 去重和抽取后的数据维度
+print(df_out.shape)  # (20019035, 5)
+
+# 输出为csv
+df_out.to_csv('UserBehavior_20.csv', index=False, header=False)
+
+
 
 def single_user_behavior_statistics(df, user_id, top_n=5):
     """统计单个用户行为数据
@@ -18,8 +61,8 @@ def single_user_behavior_statistics(df, user_id, top_n=5):
             }
         }
     """
-    assert isinstance(user_id, int), "User id type uncorrect!"
-    assert isinstance(top_n, int) and top_n > 0 and top_n < 10, "top_n uncorrect!"
+    assert isinstance(user_id, int), "用户名输入有误!"
+    assert isinstance(top_n, int) and top_n > 0 and top_n < 10, "商品类型出错!"
     
     df_tmp = df.loc[df['用户ID'] == user_id, :]
     
@@ -150,6 +193,7 @@ def who_cart_most(df, top_n=1000):
     
     return result
 
+
 def who_fav_most(df, top_n=1000):
     """行为为fav的所有item中，排名前top_n个用户
 
@@ -169,3 +213,66 @@ def who_fav_most(df, top_n=1000):
         result.append(['fav', user_id, userid_num[user_id]])
     
     return result
+#pv-cart-buy转化率
+"""建立一个转化率字典dic_conv，其中有些项是空的，随后要填充数据进去"""
+dic_conv = {
+    'Step':['pv','cart','buy'],
+    'Num':[],
+    'Conv':[],
+    'Conv_total':[]
+}
+"""计算每一步的数据量"""
+pv_num = data[data['BehaviorType']=='pv']['UserID'].count()
+cart_num = data[data['BehaviorType']=='cart']['UserID'].count()
+buy_num = data[data['BehaviorType']=='buy']['UserID'].count()
+"""计算转化率"""
+pv2pv = round((pv_num/pv_num),4)
+pv2cart = round((cart_num/pv_num),4)
+cart2buy = round((buy_num/cart_num),4)
+pv2buy = round((buy_num/pv_num),4)
+"""更新table_conv中的每项数据'Num'列"""
+dic_conv['Num'].append(pv_num)
+dic_conv['Num'].append(cart_num)
+dic_conv['Num'].append(buy_num)
+"""更新table_conv中的每步与上一步的转化率'Conv'列"""
+dic_conv['Conv'].append(pv2pv)
+dic_conv['Conv'].append(pv2cart)
+dic_conv['Conv'].append(cart2buy)
+"""更新table_conv中的截至每步的总转化率'Conv_total'列"""
+dic_conv['Conv_total'].append(pv2pv)
+dic_conv['Conv_total'].append(pv2cart)
+dic_conv['Conv_total'].append(pv2buy)
+pd.DataFrame(dic_conv) #将结果转化为DaraFrame格式，并输出结果
+#pv-fav-buy转化率
+"""建立一个转化率字典dic_conv，其中有些项是空的，随后要填充数据进去"""
+dic_conv = {
+    'Step':['pv','fav','buy'],
+    'Num':[],
+    'Conv':[],
+    'Conv_total':[]
+}
+"""计算每一步的数据量"""
+pv_num = data[data['BehaviorType']=='pv']['UserID'].count()
+fav_num = data[data['BehaviorType']=='fav']['UserID'].count()
+buy_num = data[data['BehaviorType']=='buy']['UserID'].count()
+"""计算转化率"""
+pv2pv = round((pv_num/pv_num),4)
+pv2fav = round((fav_num/pv_num),4)
+fav2buy = round((buy_num/fav_num),4)
+pv2buy = round((buy_num/pv_num),4)
+"""更新table_conv中的每项数据'Num'列"""
+dic_conv['Num'].append(pv_num)
+dic_conv['Num'].append(fav_num)
+dic_conv['Num'].append(buy_num)
+"""更新table_conv中的每步与上一步的转化率'Conv'列"""
+dic_conv['Conv'].append(pv2pv)
+dic_conv['Conv'].append(pv2fav)
+dic_conv['Conv'].append(fav2buy)
+"""更新table_conv中的截至每步的总转化率'Conv_total'列"""
+dic_conv['Conv_total'].append(pv2pv)
+dic_conv['Conv_total'].append(pv2fav)
+dic_conv['Conv_total'].append(pv2buy)
+pd.DataFrame(dic_conv) #将结果转化为DaraFrame格式，并输出结果
+
+
+
